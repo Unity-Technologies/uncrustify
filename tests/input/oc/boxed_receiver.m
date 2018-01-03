e88943a59dca26fffc2781cdc226406630079af3
Antti Tapaninen <aet@unity3d.com>
2018-01-02 17:40:48 -0800

    handle_oc_message_send: improve boxed expression support
    Fixes whitespace issues when wrapping @()/@{}/@[] receivers
    within Obj-C square brackets
    
    Similar whitespace issue when wrapping the receiver within
    parentheses, for example [(Foo *) (self.foo.bar) method]
    
    Example failure from the diff file:
    
         headers = @{
    -        @"Content-Length": [@(contentLength) stringValue],
    +        @"Content-Length":[@(contentLength)stringValue],
             @"Content-Length2": [contentLength stringValue],
             @"Content-Disposition": [NSString stringWithFormat:@"name=\"%@\"; filename=\"%@\"", @"asset_data", identifier],
             @"Content-Type": @"application/octet-stream",
    
    ..
    
     - (BOOL)isStrimmed {
    -    if ([(TestClassVideoEditorView *) (self.parentComponentsView.superview) isTrimmed]) {
    +    if ([(TestClassVideoEditorView *) (self.parentComponentsView.superview)isTrimmed]) {
             return YES;
         }
         if ([((TestClassVideoEditorView *) self.parentComponentsView.superview) isTrimmed]) {

---
+++ tests/input/oc/boxed_receiver.m
@@ -0,0 +1,35 @@
+#import <Foundation/Foundation.h>
+
+@implementation TestClass
+
+- (NSDictionary *)constructUploadHeaders {
+    NSDictionary *headers;
+
+    headers = @{
+        @"Content-Length":[@(contentLength)stringValue],
+        @"Content-Length2": [contentLength stringValue],
+        @"Content-Disposition": [NSString stringWithFormat:@"name=\"%@\"; filename=\"%@\"", @"asset_data", identifier],
+        @"Content-Type": @"application/octet-stream",
+        @"ETag": uploadEtag
+    };
+
+    return headers;
+}
+
+- (BOOL)isStrimmed {
+    if ([(TestClassVideoEditorView *)   (self.parentComponentsView.superview)isTrimmed]) {
+    	return YES;
+    }
+    if ([((TestClassVideoEditorView *)   self.parentComponentsView.superview)isTrimmed]) {
+    	return YES;
+    }
+    return NO;
+}
+
+- (void)session {
+    TestCaseSessionInstance *session = ((TestClassVideoEditorView *)  self.parentComponentsView.superview).session;
+
+    [(TestClassVideoEditorView *)   (  self.parentComponentsView.superview) closeEditor];
+}
+
+@end
