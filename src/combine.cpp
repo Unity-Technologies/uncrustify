90d9280373f4a43b21860cbde240b9541542895e
Antti Tapaninen <aet@unity3d.com>
2018-01-03 14:07:09 -0800

    improve fix_casts for Java/Obj-C
    Certain type casts (generics/protocols) cause whitespace
    issues for both Java/Obj-C, with Java you can also refer to
    a class with a member
    
    Example failure from the diff file:
    
    Java:
    
    -            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) imageView.getLayoutParams();
    +            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)imageView.getLayoutParams();
    
    -        Map< ? , ? > map = (Map< ? , ? >) object;
    +        Map< ? , ? > map = (Map< ? , ? >)object;
    
    -        return (List<Object>) fromJSON((Object) obj);
    +        return (List<Object>)fromJSON((Object) obj);
    
    Obj-C:
    
    -    previewViewController.previewControllerDelegate = (id<TestClassDelegate>) [TestClass sharedInstance];
    +    previewViewController.previewControllerDelegate = (id<TestClassDelegate>)[TestClass sharedInstance];

--- src/combine.cpp
+++ src/combine.cpp
@@ -2542,6 +2542,8 @@ static void fix_casts(chunk_t *start)
             || pc->type == CT_QUESTION
             || pc->type == CT_CARET
             || pc->type == CT_TSQUARE
+            || ((pc->type == CT_ANGLE_OPEN || pc->type == CT_ANGLE_CLOSE) && (cpd.lang_flags & (LANG_OC | LANG_JAVA)))
+            || ((pc->type == CT_QUESTION || pc->type == CT_COMMA || pc->type == CT_MEMBER) && (cpd.lang_flags & (LANG_JAVA)))
             || pc->type == CT_AMP))
    {
       LOG_FMT(LCASTS, " [%s]", get_token_name(pc->type));
@@ -2550,7 +2552,7 @@ static void fix_casts(chunk_t *start)
       {
          word_count++;
       }
-      else if (pc->type == CT_DC_MEMBER || pc->type == CT_PP)
+      else if (pc->type == CT_DC_MEMBER || pc->type == CT_MEMBER || pc->type == CT_PP)
       {
          word_count--;
       }
@@ -2580,7 +2582,8 @@ static void fix_casts(chunk_t *start)
    if (  last->type == CT_STAR
       || last->type == CT_CARET
       || last->type == CT_PTR_TYPE
-      || last->type == CT_TYPE)
+      || last->type == CT_TYPE
+      || (last->type == CT_ANGLE_CLOSE && (cpd.lang_flags & (LANG_OC | LANG_JAVA))))
    {
       verb = "for sure";
    }
