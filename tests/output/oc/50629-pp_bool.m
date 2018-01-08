Unity-Technologies/uncrustify

commit 59fd03e966e23b6a2bbe1b599a9e1bc1484ad485

a bugfix for #548

src/combine.cpp
@@ -1734,6 +1734,15 @@ void do_symbol_check(chunk_t *prev, chunk_t *pc, chunk_t *next)
          }
       }
    }
+
+   // Issue #548
+   // inline T && someFunc(foo * *p, bar && q) { }
+   if ((pc->type == CT_BOOL) &&
+       chunk_is_str(pc, "&&", 2) &&
+       chunk_ends_type(pc->prev))
+   {
+      set_chunk_type(pc, CT_BYREF);
+   }
 } // do_symbol_check
 
 

tests/cpp.test
@@ -318,6 +318,7 @@
 33052 empty.cfg                        cpp/bug_i_512.cpp
 33053 for_auto.cfg                     cpp/for_auto.cpp
 33054 bug_i_825.cfg                    cpp/bug_i_825.cpp
+33056 empty.cfg                        cpp/bug_33056.cpp
 33061 empty.cfg                        cpp/if_chain_braces.cpp
 33062 if_chain_braces_0.cfg            cpp/if_chain_braces.cpp
 33063 if_chain_braces_1.cfg            cpp/if_chain_braces.cpp

tests/input/cpp/bug_33056.cpp
@@ -1,0 +1,7 @@
+inline T* * someFunc(foo** p, bar&& q)
+{
+}
+
+inline T && someFunc(foo * *p, bar && q)
+{
+}

tests/output/cpp/33056-bug_33056.cpp
@@ -1,0 +1,7 @@
+inline T* * someFunc(foo** p, bar&& q)
+{
+}
+
+inline T && someFunc(foo * *p, bar && q)
+{
+}

commit 4ec2bdb37c1e366b2a352fc24031637dca4c4b48

fix CT_BOOL regression from 59fd03e966e23b6a2bbe1b599a9e1bc1484ad485
in the current master, it seems this change is no longer required
to fix issue #548 along the testcase for it (tests/input/cpp/bug_33056.cpp)

fixes regression with:

 #import <Foundation/Foundation.h>

-#if TARGET_OS_MAC && !TARGET_OS_IPHONE
+#if TARGET_OS_MAC&& !TARGET_OS_IPHONE
 #import <AppKit/AppKit.h>
 #endif

src/combine.cpp
@@ -1797,14 +1797,6 @@ void do_symbol_check(chunk_t *prev, chunk_t *pc, chunk_t *next)
          }
       }
    }
-
-   // Issue #548: inline T && someFunc(foo * *p, bar && q) { }
-   if (  pc->type == CT_BOOL
-      && chunk_is_str(pc, "&&", 2)
-      && chunk_ends_type(pc->prev))
-   {
-      set_chunk_type(pc, CT_BYREF);
-   }
 } // do_symbol_check
 
 

tests/input/oc/pp_bool.m
@@ -1,0 +1,5 @@
+#import <Foundation/Foundation.h>
+
+#if TARGET_OS_MAC && !TARGET_OS_IPHONE
+#import <AppKit/AppKit.h>
+#endif

tests/objective-c.test
@@ -115,6 +115,7 @@
 50626  aet.cfg                                      oc/chunk_ends_type4.m
 50627  aet.cfg                                      oc/method_ends_semicolon.m
 50628  aet.cfg                                      oc/macro-close-brace.m
+50629  aet.cfg                                      oc/pp_bool.m
 
 50700  cmt_insert-0.cfg                             oc/cmt_insert.m
 

tests/output/oc/50629-pp_bool.m
@@ -1,0 +1,5 @@
+#import <Foundation/Foundation.h>
+
+#if TARGET_OS_MAC && !TARGET_OS_IPHONE
+#import <AppKit/AppKit.h>
+#endif